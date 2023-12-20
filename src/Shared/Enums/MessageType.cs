f7fcb664fcbe1cfbc38b503ddcf07ca55436a9c0
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-12-20 13:27:17 -0300

    chore: client and messagetype

---
+++ src/Shared/Enums/MessageType.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum MessageType
+{
+    Message,
+    Notification,
+}
---
+++ src/Shared/Models/Client.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Models;
+
+public class Client
+{
+    /// <summary>
+    /// <para>Automatically generated</para>
+    /// <para>The unique identifier of the client.</para>
+    /// </summary>
+    [JsonPropertyName("id")]
+    public static Guid Id => Guid.NewGuid();
+
+    /// <summary>
+    /// <para>Required</para>
+    /// <para>The nickname of the client.</para>
+    /// </summary>
+    [JsonPropertyName("nickname")]
+    public required string Nickname { get; init; }
+
+    /// <summary>
+    /// <para>Automatically generated</para>
+    /// <para>The color of the client's nickname appearing in the chat.</para>
+    /// </summary>
+    [JsonPropertyName("color")]
+    public static string Color => GenerateRandomHexColor();
+
+    /// <summary>
+    /// <para>Required</para>
+    /// <para>The room the client is in, used to send messages to the correct room.</para>
+    /// </summary>
+    [JsonPropertyName("room")]
+    public required string Room { get; init; }
+
+    private static string GenerateRandomHexColor()
+    {
+        var random = new Random();
+        var color = string.Format("#{0:X6}", random.Next(0x1000000));
+
+        if (color.Length != 7)
+        {
+            color = color.PadLeft(7, '0');
+        }
+
+        return color;
+    }
+}
